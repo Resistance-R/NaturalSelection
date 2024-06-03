bc3132393b8dc7f480887df7c95fe7f0fcdd23d1
Resistance-R <125963196+Resistance-R@users.noreply.github.com>
2024-06-03 15:03:54 +0900

    add movement

--- Scenes/predator.gd
+++ Scenes/predator.gd
@@ -1,10 +1,15 @@
 extends RigidBody2D
 
-@export var velocity = Vector2(randf_range(-10, 10), randf_range(-10, 10))
+var velocity
 
 func _ready():
+	velocity = Vector2(randi_range(-10, 10), randi_range(-10, 10))
 	add_to_group("predator")
 
 func _process(delta):
 	move_and_collide(velocity)
-	
+
+func _on_body_entered(body):
+	print("entered")
+	velocity = Vector2(randi_range(-10, 10), randi_range(-10, 10))
+	print("vetor changed")
--- Scenes/prey.gd
+++ Scenes/prey.gd
@@ -3,11 +3,16 @@ extends RigidBody2D
 var velocity
 
 func _ready():
-	velocity = Vector2(randf_range(-5, 5), randf_range(-5, 5))
+	velocity = Vector2(randi_range(-5, 5), randi_range(-5, 5))
 	add_to_group("prey")
 
 func _physics_process(delta):
 	move_and_collide(velocity)
 
 func _on_body_entered(body):
-	velocity = Vector2(randf_range(-5, 5), randf_range(-5, 5))
+	print("entered")
+	velocity = Vector2(randi_range(-5, 5), randi_range(-5, 5))
+	print("vetor changed")
+
+	if(body.is_in_group("predator")):
+		queue_free()
